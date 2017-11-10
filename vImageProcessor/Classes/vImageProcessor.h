04a5e203635c66653048bdd20ae1bc92f26e8508
DreamPiggy <lizhuoli1126@126.com>
2017-11-11 04:10:48 +0800

    Add nullability attibutes

--- vImageProcessor/Classes/vImageProcessor.h
+++ vImageProcessor/Classes/vImageProcessor.h
@@ -9,27 +9,31 @@
 #import <Foundation/Foundation.h>
 #import <CoreGraphics/CoreGraphics.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 // vImage for image processing
-// Returned CGImage are new created and you are responsed to free it by `CGImageRelease` after usage.
+// Returned CGImage are all new created following `The Create Rule`. You are responsed to free it by `CGImageRelease` after usage.
 // These methods use Core Graphics coordinate system instead of UIKit coordinate system, which is Y-axis positive. Ensure your call with offset, rect, affine transform with the correct coordinate system.
 
 @interface vImageProcessor : NSObject
 
 // apply alpha blending for color
-+ (CGImageRef)alphaBlendedImageWithImage:(CGImageRef)aImage color:(CGColorRef)color;
++ (CGImageRef _Nullable)alphaBlendedImageWithImage:(CGImageRef)aImage color:(CGColorRef)color;
 // apply alpha blending for image
-+ (CGImageRef)alphaBlendedImageWithImage:(CGImageRef)aImage image:(CGImageRef)bImage offset:(CGVector)offset;
++ (CGImageRef _Nullable)alphaBlendedImageWithImage:(CGImageRef)aImage image:(CGImageRef)bImage offset:(CGVector)offset;
 // apply scale for size
-+ (CGImageRef)scaledImageWithImage:(CGImageRef)aImage size:(CGSize)size;
++ (CGImageRef _Nullable)scaledImageWithImage:(CGImageRef)aImage size:(CGSize)size;
 // apply cropping for rect
-+ (CGImageRef)croppedImageWithImage:(CGImageRef)aImage rect:(CGRect)rect;
++ (CGImageRef _Nullable)croppedImageWithImage:(CGImageRef)aImage rect:(CGRect)rect;
 // apply vertical or horizontal flipping
-+ (CGImageRef)flippedImageWithImage:(CGImageRef)aImage horizontal:(BOOL)horizontal;
++ (CGImageRef _Nullable)flippedImageWithImage:(CGImageRef)aImage horizontal:(BOOL)horizontal;
 // apply rotation for radians
-+ (CGImageRef)rotatedImageWithImage:(CGImageRef)aImage radians:(CGFloat)radians;
++ (CGImageRef _Nullable)rotatedImageWithImage:(CGImageRef)aImage radians:(CGFloat)radians;
 // apply vertical or horizontal shearing
-+ (CGImageRef)shearedImageWithImage:(CGImageRef)aImage horizontal:(BOOL)horizontal offset:(CGVector)offset translation:(CGFloat)translation slope:(CGFloat)slope scale:(CGFloat)scale;
++ (CGImageRef _Nullable)shearedImageWithImage:(CGImageRef)aImage horizontal:(BOOL)horizontal offset:(CGVector)offset translation:(CGFloat)translation slope:(CGFloat)slope scale:(CGFloat)scale;
 // apply affine transform
-+ (CGImageRef)affineTransformedImageWithImage:(CGImageRef)aImage transform:(CGAffineTransform)transform;
++ (CGImageRef _Nullable)affineTransformedImageWithImage:(CGImageRef)aImage transform:(CGAffineTransform)transform;
 
 @end
+
+NS_ASSUME_NONNULL_END
